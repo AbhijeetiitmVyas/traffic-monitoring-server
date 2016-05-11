3b81d95ac6a4c96347e077ae59de69b10add09e7
AbhijeetiitmVyas <abhijeet3vyas@gmail.com>
2016-05-11 23:19:14 +0530

    Makes the output as a list of lists and is valid for 999 nodes at max and 1000 time stamps.

--- TMP.py
+++ TMP.py
@@ -1,14 +1,14 @@
 import math 
 import csv
 import numpy as np
-output=np.zeros((1000,36))
+output=np.zeros((1000,998001))
 k=0
 a=[]
 s=0  #format of output is such that the time for i-j node  is the (6*(i-1)+j)th bit for the kth time stamp.0 =>no data
 x=np.array(list(csv.reader(open('C:/Users/Abhijeet/Downloads/traffic_data.csv', 'rb'),delimiter=','))).astype(float)   
 max_range=len(x)
 for i in range(max_range):
-    a.append(x[i][0]*100+x[i][1]*10+x[i][2])
+    a.append(x[i][0]*1000000+x[i][1]*1000+x[i][2])
 
 for i in range(max_range):
     k=0
